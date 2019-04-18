4f1bdaef50c85c5d50e553711571d9993879daa3
Jonghyun Kim <jhkim@dit.ac.kr>
2019-04-18 13:16:16 +0900

    column 2개 구분

--- PickerView Test/ViewController.swift
+++ PickerView Test/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-    var myColor = ["red", "green", "blue"]
+    var myColor = ["red", "green", "blue", "white", "black"]
+    var myEmotion = ["Angry", "Calm", "Cool", "Innocent", "Fear"]
     @IBOutlet weak var outLabel: UILabel!
     @IBOutlet weak var myPickerView: UIPickerView!
+    var count = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,20 +28,43 @@ class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSo
     
     // UIPickerViewDataSource 메소드 호출
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
-        return 1
+        return 2
     }
     
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-        return myColor.count
+        if component == 0 {
+            return myColor.count
+        } else {
+            return myEmotion.count
+        }
     }
     
     // UIPickerViewDelegate 메소드 호출
     public func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
-        return myColor[row]
+        if component == 0 {
+            return myColor[row]
+        } else {
+            return myEmotion[row]
+        }
     }
 
     public func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        outLabel.text = myColor[row]
+        
+        if component == 0 {
+            print("Component = \(component)")
+            print(row)
+            print("----------")
+            outLabel.text = myColor[row]
+        } else {
+            print("Component = \(component)")
+            print(row)
+            print("----------")
+            outLabel.text = myEmotion[row]
+        }
+    }
+    
+    public func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+        return 80.0
     }
 }
 
